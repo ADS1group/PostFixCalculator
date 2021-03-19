ef627cc3dd4e43fc6106a7ffc2f8b194678feb2c
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-03-19 20:40:06 +0100

    2040

--- src/main/java/CalculatorVisitor.java
+++ src/main/java/CalculatorVisitor.java
@@ -1,4 +1,28 @@
-public class CalculatorVisitor {
-   // LinkedStack<Token> tokenStack;
+public class CalculatorVisitor extends Operand implements Visitor, Calculator {
+    private LinkedStack<Token> tokenStack;
+
+    private void pushOperand(Operand operand) {
+
+    }
+
+    private void performOperation(Operator operator) {
+        
+    }
+
+    @Override
+    public int getResult() {
+        return 0;
+    }
+
+    @Override
+    public void visit(Operand operand) {
+
+    }
+
+    @Override
+    public void visit(Operator operator) {
+
+    }
+
 
 }
